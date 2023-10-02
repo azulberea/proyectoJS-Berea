6c416601f161c6566f3de08101ddbfe4f9785d66
azulberea <azul.berea16.am@gmail.com>
2023-10-02 20:08:49 -0300

    agregue arrays y objetos para 2da entrega

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ let numProducto = parseInt(prompt(`En cual de nuestros productos estas interesad
             bebidaAlcoholica.push(parseInt(prompt(`Qué bebida alcohólica estás buscando, ${nombre}? \n1- Gin \n2- Vodka \n3- Fernet \n4- Ron blanco`)));
             break;
         case 4:
-            bebidaNoAlcoholica.push(parseInt(prompt(`Qué bebida no alcohólica estás buscando, ${nombre}? \n1- Agua tónica \n2- Energizante \n3- Coca-Cola \n4- Jugo de pomelo`)));
+            bebidaNoAlcoholica.push(parseInt(prompt(`Qué bebida no alcohólica estás buscando, ${nombre}? \n1- Agua tónica \n2- Energizante \n3- Coca-Cola \n4- Gaseosa de pomelo`)));
             break;
         default:
             alert("Ingresa un número válido, por favor");
@@ -39,46 +39,46 @@ let numProducto = parseInt(prompt(`En cual de nuestros productos estas interesad
 const productosCarrito = []
 for(let x of utensilio){
     if(x == 1){
-        productosCarrito.push({categoria:"Utensilios", item:"Barspoon", precio:1000})
+        productosCarrito.push({categoria:"Utensilios", item:"Barspoon", precio:4000})
     }else if(x == 2){
-        productosCarrito.push({categoria:"Utensilios", item:"Coctelera", precio:1000})
+        productosCarrito.push({categoria:"Utensilios", item:"Coctelera", precio:6600})
     }else if(x == 3){
-        productosCarrito.push({categoria:"Utensilios", item:"Colador oruga", precio:1000})
+        productosCarrito.push({categoria:"Utensilios", item:"Colador oruga", precio:3700})
     }else if(x == 4){
-        productosCarrito.push({categoria:"Utensilios", item:"Jigger", precio:1000})
+        productosCarrito.push({categoria:"Utensilios", item:"Jigger", precio:3250})
     }
 }
 for(let x of cristaleria){
     if(x == 1){
-        productosCarrito.push({categoria:"Cristaleria", item:"Vaso Old Fashioned", precio:1000})
+        productosCarrito.push({categoria:"Cristaleria", item:"Vaso Old Fashioned", precio:1590})
     }else if(x == 2){
-        productosCarrito.push({categoria:"Cristaleria", item:"Vaso Highball", precio:1000})
+        productosCarrito.push({categoria:"Cristaleria", item:"Vaso Highball", precio:1350})
     }else if(x == 3){
-        productosCarrito.push({categoria:"Cristaleria", item:"Vaso Hurricane", precio:1000})
+        productosCarrito.push({categoria:"Cristaleria", item:"Vaso Hurricane", precio:2450})
     }else if(x == 4){
-        productosCarrito.push({categoria:"Cristaleria", item:"Copa Cocktail", precio:1000})
+        productosCarrito.push({categoria:"Cristaleria", item:"Copa Cocktail", precio:2750})
     }
 }
 for(let x of bebidaAlcoholica){
     if(x == 1){
-        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Gin", precio:1000})
+        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Gin", precio:3500})
     }else if(x == 2){
-        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Vodka", precio:1000})
+        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Vodka", precio:2400})
     }else if(x == 3){
-        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Fernet", precio:1000})
+        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Fernet", precio:4450})
     }else if(x == 4){
-        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Ron blanco", precio:1000})
+        productosCarrito.push({categoria:"Bebidas alcohólicas", item:"Ron blanco", precio:6700})
     }
 }
 for(let x of bebidaNoAlcoholica){
     if(x == 1){
-        productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Agua tónica", precio:1000})
+        productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Agua tónica", precio:750})
     }else if(x == 2){
-        productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Energizante", precio:1000})
+        productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Energizante", precio:850})
     }else if(x == 3){
         productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Coca-Cola", precio:1000})
     }else if(x == 4){
-        productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Jugo de pomelo", precio:1000})
+        productosCarrito.push({categoria:"Bebidas no alcohólicas", item:"Gaseosa de pomelo", precio:990})
     }
 }
 
@@ -86,4 +86,4 @@ const mostrarProductosCarrito = []
 for(const x of productosCarrito){
     mostrarProductosCarrito.push(x.item)
 }
-alert(`¡Muchas gracias por la compra de tu ${mostrarProductosCarrito.join(", ")}, ${nombre}`)
+alert(`¡Muchas gracias por la compra de tu ${mostrarProductosCarrito.join(", ")}, ${nombre}!`)
